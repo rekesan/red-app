1d9a26a8822a34258a9bff11419bf1de1e9f6b9e
rekesan <redkeans@gmail.com>
2023-02-28 13:04:35 +0800

    can now stop the music if pressed again

--- src/components/DisplayAnimals.js
+++ src/components/DisplayAnimals.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import "../styles/DisplayAnimals.css";
 import useSound from "use-sound";
 
 export const DisplayAnimals = ({ name, image, audio }) => {
-  const [play] = useSound(audio);
+  const [play, { stop }] = useSound(audio);
+  const [isPlaying, setIsPlay] = useState(false);
+  
+  const handleClick = () => {
+    if (isPlaying) {
+      stop();
+      setIsPlay(false);
+    } else {
+      play();
+      setIsPlay(true);
+    }
+  };
+
   return (
-    <button className="list" onClick={play}>
+    <button className={`${isPlaying ? "playing" : "list"}`} onClick={handleClick}>
       {image} :: {name}
     </button>
   );
